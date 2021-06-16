df454ecc3da8897d4b15ede10419c8b76800e1e0
Andre Bruch <ab@andrebruch.com>
2021-06-16 21:01:38 +0200

    add versioning tests (retention - multipart)

--- build/versioning/main.go
+++ build/versioning/main.go
@@ -127,7 +127,6 @@ func main() {
 	testLockingLegalholdMultipart()
 	testPutGetRetentionCompliance()
 	testPutGetDeleteRetentionGovernance()
-	testPutGetDeleteRetentionGovernanceMultipart()
 	testLockingRetentionGovernance()
 	testLockingRetentionGovernanceMultipart()
 	testLockingRetentionCompliance()
