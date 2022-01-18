273dea0533a689e089bfe49dbc60e31b97b96eed
Tristan Baumbusch <tristan.baumbusch@essquare.de>
2022-01-18 09:51:45 +0100

    Added new section 'ilm'.
    The new ilm section contains basic tests for deletion rules.

---
+++ build/ilm/deletion.go
@@ -0,0 +1,196 @@
+/*
+*
+*  Mint, (C) 2021 Minio, Inc.
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*
+ */
+
+package main
+
+import (
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"io/ioutil"
+	"math/rand"
+	"strings"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+// Tests ilm deletion rules
+func testDeletionRules() {
+	lConfigFuture := &s3.BucketLifecycleConfiguration{
+		Rules: []*s3.LifecycleRule{
+			{
+				ID:     aws.String("expirydateinfuture"),
+				Status: aws.String("Enabled"),
+				Expiration: &s3.LifecycleExpiration{
+					Date: aws.Time(time.Now().UTC().Truncate(24*time.Hour).AddDate(0, 0, 1)),
+				},
+				Filter: &s3.LifecycleRuleFilter{
+					Prefix: aws.String("/"),
+				},
+			},
+		},
+	}
+
+	lConfigPast := &s3.BucketLifecycleConfiguration{
+		Rules: []*s3.LifecycleRule{
+			{
+				ID:     aws.String("expirydateinpast"),
+				Status: aws.String("Enabled"),
+				Expiration: &s3.LifecycleExpiration{
+					Date: aws.Time(time.Now().UTC().Truncate(24*time.Hour).AddDate(0, 0, -2)),
+				},
+				Filter: &s3.LifecycleRuleFilter{
+					Prefix: aws.String("/"),
+				},
+			},
+		},
+	}
+
+	lConfigPastPrefix := &s3.BucketLifecycleConfiguration{
+		Rules: []*s3.LifecycleRule{
+			{
+				ID:     aws.String("expirydateinpast"),
+				Status: aws.String("Enabled"),
+				Expiration: &s3.LifecycleExpiration{
+					Date: aws.Time(time.Now().UTC().Truncate(24*time.Hour).AddDate(0, 0, -2)),
+				},
+				Filter: &s3.LifecycleRuleFilter{
+					Prefix: aws.String("/prefix"),
+				},
+			},
+		},
+	}
+
+	testCases := []struct {
+		lConfig     *s3.BucketLifecycleConfiguration
+		object      string
+		expDeletion bool
+	}{
+		// testDeletionRules case - 1.
+		// Expire date in future, object not deleted
+		{
+			lConfig:     lConfigFuture,
+			object:      "/object",
+			expDeletion: false,
+		},
+		// testDeletionRules case - 2.
+		// Expire date in past, rule without prefix filter
+		{
+			lConfig:     lConfigPast,
+			object:      "/object",
+			expDeletion: true,
+		},
+		// testDeletionRules case - 3.
+		// Expire date in past, rule with prefix filter does not match
+		{
+			lConfig:     lConfigPastPrefix,
+			object:      "/object",
+			expDeletion: false,
+		},
+		// testDeletionRules case - 4.
+		// Expire date in past, rule with prefix filter matches
+		{
+			lConfig:     lConfigPastPrefix,
+			object:      "/prefix/object",
+			expDeletion: true,
+		},
+	}
+
+	for i, testCase := range testCases {
+		execTestDeletionRules(i, testCase)
+	}
+
+}
+
+func execTestDeletionRules(i int, testCase struct {
+	lConfig     *s3.BucketLifecycleConfiguration
+	object      string
+	expDeletion bool
+}) {
+	// initialize logging params
+	startTime := time.Now()
+	function := "testDeletionRules"
+	bucketName := randString(60, rand.NewSource(time.Now().UnixNano()), "ilm-test-")
+	args := map[string]interface{}{
+		"testCase":    i,
+		"bucketName":  bucketName,
+		"objectName":  testCase.object,
+		"expDeletion": testCase.expDeletion,
+	}
+	_, err := s3Client.CreateBucket(&s3.CreateBucketInput{
+		Bucket: aws.String(bucketName),
+	})
+	if err != nil {
+		failureLog(function, args, startTime, "", "CreateBucket Failed", err).Fatal()
+		return
+	}
+	defer cleanup(s3Client, bucketName, testCase.object, function, args, startTime, true)
+
+	_, err = s3Client.PutBucketLifecycleConfiguration(&s3.PutBucketLifecycleConfigurationInput{
+		Bucket:                 aws.String(bucketName),
+		LifecycleConfiguration: testCase.lConfig,
+	})
+	if err != nil {
+		failureLog(function, args, startTime, "", "Put LifecycleConfiguration failed", err).Fatal()
+		return
+	}
+
+	putInput1 := &s3.PutObjectInput{
+		Body:   aws.ReadSeekCloser(strings.NewReader("my content 1")),
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(testCase.object),
+	}
+	_, err = s3Client.PutObject(putInput1)
+	if err != nil {
+		failureLog(function, args, startTime, "", "PUT expected to succeed but failed", err).Fatal()
+		return
+	}
+
+	getInput := &s3.GetObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(testCase.object),
+	}
+
+	result, err := s3Client.GetObject(getInput)
+	if err != nil {
+		aerr, ok := err.(awserr.Error)
+		if !ok {
+			failureLog(function, args, startTime, "", "Unexpected non aws error on GetObject", err).Fatal()
+			return
+		}
+		if testCase.expDeletion && aerr.Code() != s3.ErrCodeNoSuchKey || !testCase.expDeletion {
+			failureLog(function, args, startTime, "", "Unexpected aws error on GetObject", err).Fatal()
+			return
+		}
+	}
+
+	body, err := ioutil.ReadAll(result.Body)
+	if err != nil {
+		failureLog(function, args, startTime, "", "Expected to return data but failed", err).Fatal()
+		return
+	}
+	result.Body.Close()
+
+	if string(body) != "my content 1" {
+		failureLog(function, args, startTime, "", "Unexpected body content", err).Fatal()
+		return
+	}
+
+	successLogger(function, args, startTime).Info()
+}
